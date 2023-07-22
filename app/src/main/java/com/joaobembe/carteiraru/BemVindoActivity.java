86171c515ed856a5b25981533693ac3cbab8e6d8
Joao Paulo S Bembe <joaobembe@icloud.com>
2023-07-21 22:36:51 -0300

    LoginActivity, BemVindoActivity

---
+++ app/src/main/java/com/joaobembe/carteiraru/BemVindoActivity.java
@@ -0,0 +1,14 @@
+package com.joaobembe.carteiraru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BemVindoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bem_vindo);
+    }
+}
