eb1b51b748cfa40d17c7a1360fee9484e9cdf1bc
Joao Paulo S Bembe <joaobembe@icloud.com>
2023-08-03 23:00:42 -0300

    atualização/ melhorias na interface

--- app/src/main/java/com/joaobembe/carteiraru/LoginActivity.java
+++ app/src/main/java/com/joaobembe/carteiraru/LoginActivity.java
@@ -72,64 +72,80 @@ public class LoginActivity extends AppCompatActivity {
         etUsuario.setText(sharedPreferences.getString("usuario", ""));
         etSenha.setText(sharedPreferences.getString("senha", ""));
         button.setOnClickListener(view -> {
-            ExecutorService service = Executors.newSingleThreadExecutor();
-            service.execute(() -> {
-                try {
-                    runOnUiThread(() -> {
-                        etSenha.setEnabled(false);
-                        etUsuario.setEnabled(false);
-                        button.setVisibility(View.GONE);
-                        progressBar.setVisibility(View.VISIBLE);
-                        tvUsuarioSenhaInvalido.setVisibility(View.INVISIBLE);
-                        etSenha.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
-                        etUsuario.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
-                    });
 
-                    new Login().fazerLogin(new Credenciais(etUsuario.getText().toString(), etSenha.getText().toString()));
+            if (etUsuario.getText().toString().equals("") && etSenha.getText().toString().equals("")) {
+                etUsuario.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(LoginActivity.this, R.drawable.baseline_error_24), null);
+                tvUsuarioSenhaInvalido.setText("Informe o usuário e senha!");
+                tvUsuarioSenhaInvalido.setVisibility(View.VISIBLE);
+            } else if (etSenha.getText().toString().equals("")){
+                etSenha.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(LoginActivity.this, R.drawable.baseline_error_24), null);
+                tvUsuarioSenhaInvalido.setText("Informe a senha!");
+                tvUsuarioSenhaInvalido.setVisibility(View.VISIBLE);
+            } else if (etUsuario.getText().toString().equals("")) {
+                etUsuario.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(LoginActivity.this, R.drawable.baseline_error_24), null);
+                tvUsuarioSenhaInvalido.setText("Informe o usuário!");
+                tvUsuarioSenhaInvalido.setVisibility(View.VISIBLE);
+            } else {
+                ExecutorService service = Executors.newSingleThreadExecutor();
+                service.execute(() -> {
+                    try {
+                        runOnUiThread(() -> {
+                            etSenha.setEnabled(false);
+                            etUsuario.setEnabled(false);
+                            button.setVisibility(View.GONE);
+                            progressBar.setVisibility(View.VISIBLE);
+                            tvUsuarioSenhaInvalido.setVisibility(View.INVISIBLE);
+                            etSenha.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+                            etUsuario.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+                        });
 
-                    if (checkBox.isChecked()){
-                        String masterKeyAlias1;
-                        masterKeyAlias1 = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC);
-                        SharedPreferences sharedPreferences1;
-                        sharedPreferences1 = EncryptedSharedPreferences.create(
-                                "file",
-                                masterKeyAlias1,
-                                context,
-                                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-                                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-                        );
-                        SharedPreferences.Editor editor = sharedPreferences1.edit();
-                        editor.putString("usuario", etUsuario.getText().toString());
-                        editor.putString("senha", etSenha.getText().toString());
-                        editor.apply();
+                        new Login().fazerLogin(new Credenciais(etUsuario.getText().toString(), etSenha.getText().toString()));
+
+                        if (checkBox.isChecked()){
+                            String masterKeyAlias1;
+                            masterKeyAlias1 = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC);
+                            SharedPreferences sharedPreferences1;
+                            sharedPreferences1 = EncryptedSharedPreferences.create(
+                                    "file",
+                                    masterKeyAlias1,
+                                    context,
+                                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+                            );
+                            SharedPreferences.Editor editor = sharedPreferences1.edit();
+                            editor.putString("usuario", etUsuario.getText().toString());
+                            editor.putString("senha", etSenha.getText().toString());
+                            editor.apply();
+                        }
+                        Intent intent = new Intent(LoginActivity.this, PaginaInicialActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } catch (ExcecaoUsuarioSenhaInvalido e) {
+                        runOnUiThread(() -> {
+                            etSenha.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(LoginActivity.this, R.drawable.baseline_error_24), null);
+                            etUsuario.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(LoginActivity.this, R.drawable.baseline_error_24), null);
+                            etSenha.setEnabled(true);
+                            etUsuario.setEnabled(true);
+                            button.setVisibility(View.VISIBLE);
+                            progressBar.setVisibility(View.GONE);
+                            tvUsuarioSenhaInvalido.setText("Usuário e/ou senha invalidos!");
+                            tvUsuarioSenhaInvalido.setVisibility(View.VISIBLE);
+                        });
+                    } catch (ExcecaoErroDeConectividade e) {
+                        runOnUiThread(() -> {
+                            tvUsuarioSenhaInvalido.setText("Verifique sua conexão com a internet!");
+                            tvUsuarioSenhaInvalido.setVisibility(View.VISIBLE);
+                            etSenha.setEnabled(true);
+                            etUsuario.setEnabled(true);
+                            button.setVisibility(View.VISIBLE);
+                            progressBar.setVisibility(View.GONE);
+                        });
+                    } catch (GeneralSecurityException | IOException e) {
+                        throw new RuntimeException(e);
                     }
-                    Intent intent = new Intent(LoginActivity.this, PaginaInicialActivity.class);
-                    startActivity(intent);
-                    finish();
-                } catch (ExcecaoUsuarioSenhaInvalido e) {
-                    runOnUiThread(() -> {
-                        etSenha.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(LoginActivity.this, R.drawable.baseline_error_24), null);
-                        etUsuario.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(LoginActivity.this, R.drawable.baseline_error_24), null);
-                        etSenha.setEnabled(true);
-                        etUsuario.setEnabled(true);
-                        button.setVisibility(View.VISIBLE);
-                        progressBar.setVisibility(View.GONE);
-                        tvUsuarioSenhaInvalido.setText("Usuário e/ou senha invalidos!");
-                        tvUsuarioSenhaInvalido.setVisibility(View.VISIBLE);
-                    });
-                } catch (ExcecaoErroDeConectividade e) {
-                    runOnUiThread(() -> {
-                        tvUsuarioSenhaInvalido.setText("Verifique sua conexão com a internet!");
-                        tvUsuarioSenhaInvalido.setVisibility(View.VISIBLE);
-                        etSenha.setEnabled(true);
-                        etUsuario.setEnabled(true);
-                        button.setVisibility(View.VISIBLE);
-                        progressBar.setVisibility(View.GONE);
-                    });
-                } catch (GeneralSecurityException | IOException e) {
-                    throw new RuntimeException(e);
-                }
-            });
+                });
+            }
+
         });
 
     }
